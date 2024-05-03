d3229576ff63cad42717606ca54577ddfea523d4
mm0832-rajesh <rajesh.bhuyan@maventic.com>
2024-05-03 16:18:19 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ManagerScreen from './components/ManagerScreen';
+
+function App() {
+  return (
+    <div className="App">
+     <ManagerScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/ManagerScreen.jsx
@@ -0,0 +1,234 @@
+import React, { useState } from "react";
+import "./managerScreen.css";
+
+const ManagerScreen = () => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [assignBtn, setAssignBtn] = useState(false);
+  const [editingMode, setEditingMode] = useState(false);
+  const [editData, setEditData] = useState(null);
+
+  const [data, setData] = useState({
+    employeeId: "",
+    employeeName: "",
+    taskName: "",
+    startDate: "",
+    endDate: "",
+    planedHour: 0,
+    billableHour: 0,
+  });
+
+  const [saveData, setSaveData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const togglePopup = () => {
+    setData({
+      employeeId: "",
+      employeeName: "",
+      taskName: "",
+      startDate: "",
+      endDate: "",
+      planedHour: 0,
+      billableHour: 0,
+    });
+    setShowPopup(!showPopup);
+    setEditingMode(false);
+    setEditData(null);
+    setAssignBtn(!assignBtn)
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const saveHandler = () => {
+    if (
+      !data.employeeId ||
+      !data.employeeName ||
+      !data.taskName ||
+      !data.startDate ||
+      !data.endDate
+    ) {
+      setErrorMessage("Please fill in all required fields.");
+    } else if (new Date(data.endDate) < new Date(data.startDate)) {
+      setErrorMessage("End date cannot be less than start date.");
+    } else {
+      if (editingMode) {
+        const updatedData = saveData.map((item) =>
+          item.employeeId === editData.employeeId ? data : item
+        );
+        setSaveData(updatedData);
+        setAssignBtn(!assignBtn)
+      } else {
+        setSaveData([...saveData, data]);
+      }
+      setAssignBtn(!assignBtn)
+      setShowPopup(false);
+      setErrorMessage("");
+      setData({
+        employeeId: "",
+        employeeName: "",
+        taskName: "",
+        startDate: "",
+        endDate: "",
+        planedHour: 0,
+        billableHour: 0,
+      });
+    }
+  };
+
+  const editHandler = (id) => {
+    const selectedItem = saveData.find((item) => item.employeeId === id);
+    setData(selectedItem);
+    setEditingMode(true);
+    setShowPopup(true);
+    setEditData(selectedItem);
+  };
+
+  return (
+    <div className="container">
+      <div className="header">
+        <button className="task-btn" onClick={togglePopup} disabled={assignBtn}>
+          Assign Task
+        </button>
+      </div>
+
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <div className="popup-header">
+              <button className="close-btn" onClick={togglePopup}>
+                ❌
+              </button>
+            </div>
+            <div className="popup-body">
+              <div className="emp-task">
+                <label htmlFor="employeeId">
+                  Employee Id<span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  name="employeeId"
+                  id="employeeId"
+                  placeholder="Employee Id"
+                  required
+                  value={data.employeeId}
+                  onChange={handleChange}
+                />
+                <br />
+                <label htmlFor="employeeName">
+                  Employee Name<span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  name="employeeName"
+                  id="employeeName"
+                  placeholder="Employee Name"
+                  required
+                  value={data.employeeName}
+                  onChange={handleChange}
+                />
+                <br />
+                <label htmlFor="taskName">
+                  Task Name<span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  name="taskName"
+                  id="taskName"
+                  placeholder="Task Name"
+                  required
+                  value={data.taskName}
+                  onChange={handleChange}
+                />
+                <br />
+              </div>
+
+              <div className="task-date">
+                <label htmlFor="startDate">
+                  Start Date<span className="required">* </span>
+                </label>
+                <input
+                  type="date"
+                  name="startDate"
+                  id="startDate"
+                  value={data.startDate}
+                  onChange={handleChange}
+                />
+                <br />
+                <br />
+                <label htmlFor="endDate">
+                  End Date<span className="required">* </span>
+                </label>
+                <input
+                  type="date"
+                  name="endDate"
+                  id="endDate"
+                  value={data.endDate}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="task-hour">
+                <label htmlFor="planedHour">Planed Hours:</label>
+                <input
+                  type="number"
+                  name="planedHour"
+                  id="planedHour"
+                  value={data.planedHour}
+                  onChange={handleChange}
+                />
+                <br />
+                <br />
+                <label htmlFor="billableHour">Billable Hours:</label>
+                <input
+                  type="number"
+                  name="billableHour"
+                  id="billableHour"
+                  value={data.billableHour}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div className="popup-footer">
+              <button className="popup-btn" onClick={saveHandler}>
+                Save
+              </button>
+            </div>
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
+          </div>
+        </div>
+      )}
+
+      <table border="1" style={{ borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th>EmpId</th>
+            <th>EmpName</th>
+            <th>Task-Name</th>
+            <th>Start-Date</th>
+            <th>End-Date</th>
+            <th>Planed-Hour</th>
+            <th>Billable-Hour</th>
+          </tr>
+        </thead>
+        <tbody>
+          {saveData.map((item, index) => (
+            <tr key={index} onClick={() => editHandler(item.employeeId)}>
+              <td>{item.employeeId}</td>
+              <td>{item.employeeName}</td>
+              <td>{item.taskName}</td>
+              <td>{item.startDate}</td>
+              <td>{item.endDate}</td>
+              <td>{item.planedHour}</td>
+              <td>{item.billableHour}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ManagerScreen;
